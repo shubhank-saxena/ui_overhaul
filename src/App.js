a31bb9b1fc1c15987042b30e730901e49f18c0b4
Jay Parmani <jsparmani@gmail.com>
2020-04-17 00:29:13 +0530

    project page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {Switch, BrowserRouter as Router, Route} from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import HomeAlt from './pages/HomeAlt';
 import About from './pages/About'
+import Project from './pages/Project'
 
-const App =()=> {
+const App = () => {
   return (
     <Router>
-    <div className="App">
-    <Nav />
-    <Switch>
-    <Route exact path='/' component={HomeAlt} />
-    <Route exact path='/about' component={About} />
-    </Switch>
-    </div>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={HomeAlt} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/projects' component={Project} />
+        </Switch>
+      </div>
     </Router>
   );
 }
