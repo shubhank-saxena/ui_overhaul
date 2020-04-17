f35b21ba182c8c7df48eb27e1b4b3d82e86c7a90
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-04-17 16:15:17 +0530

    Made about page using material ui grids

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,87 +6,88 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from "styled-components";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1
+  root: {
+    flexGrow: 1
     // overflowX: 'auto',
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    appbar: {
-        // alignItems: 'center',
-        // justifyContent: 'space-around',
-        // display: 'flex',
-        //  margin: 'auto',
-        // width: '70%',
-        // paddingTop: '1%',
-        // paddingBottom: '1%',
-        // fontSize: '1.3em',
-        // fontWeight: '300'
-    },
-    toolbar: {
-        display: 'flex',
-        // paddingRight:'50%',
-        // paddingLeft:'20%'
-    },
-    button: {
-         justifyContent: 'space-around',
-        display: 'flex',
-         margin: 'auto',
-        // width: '70%',
-        // paddingTop: '1%',
-        // paddingBottom: '1%',
-        fontSize: '1.5em',
-        fontWeight: '300',
-        // paddingRight:'20%',
-        textDecoration:'none',
-        color: 'black'
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    }
-  }));
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    // alignItems: 'center',
+    // justifyContent: 'space-around',
+    // display: 'flex',
+    //  margin: 'auto',
+    // width: '70%',
+    // paddingTop: '1%',
+    // paddingBottom: '1%',
+    // fontSize: '1.3em',
+    // fontWeight: '300'
+  },
+  toolbar: {
+    display: 'flex',
+    // paddingRight:'50%',
+    // paddingLeft:'20%'
+  },
+  button: {
+    justifyContent: 'space-around',
+    display: 'flex',
+    margin: 'auto',
+    // width: '70%',
+    // paddingTop: '1%',
+    // paddingBottom: '1%',
+    fontSize: '1.5em',
+    fontWeight: '300',
+    // paddingRight:'20%',
+    textDecoration: 'none',
+    color: 'black'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  }
+}));
 
 
 const Nav = () => {
-    const classes = useStyles();
-    return (
-        <Fragment>
-        <div className={classes.root}>
-         <ResponsiveDiv>
-      <AppBar position="static" color="white" className={classes.appbar}>
-        <Toolbar className={classes.toolbar}>
-          <Link to='/' className={classes.button}>Home</Link>
-          <Link to='/about' className={classes.button}>About</Link>
-          <Link to='/' className={classes.button}>Team</Link>
-          <Link to='/events' className={classes.button}>Events</Link>
-          <Link to='/' className={classes.button}>Contact</Link>
-          <Link to='/' className={classes.button}>Achievements</Link>
-        </Toolbar>
-      </AppBar>
-      </ResponsiveDiv>
-      <MobileDiv>
-      <AppBar position="static" color='white'>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            DSC TIET
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <div className={classes.root}>
+        <ResponsiveDiv>
+          <AppBar position="static" color="white" className={classes.appbar}>
+            <Toolbar className={classes.toolbar}>
+              <Link to='/' className={classes.button}>Home</Link>
+              <Link to='/about' className={classes.button}>About</Link>
+              <Link to='/' className={classes.button}>Team</Link>
+              <Link to='/events' className={classes.button}>Events</Link>
+              <Link to='/projects' className={classes.button}>Projects</Link>
+              <Link to='/' className={classes.button}>Contact</Link>
+              <Link to='/' className={classes.button}>Achievements</Link>
+            </Toolbar>
+          </AppBar>
+        </ResponsiveDiv>
+        <MobileDiv>
+          <AppBar position="static" color='white'>
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                DSC TIET
           </Typography>
-        </Toolbar>
-      </AppBar>
-      </MobileDiv>
-    </div>
+            </Toolbar>
+          </AppBar>
+        </MobileDiv>
+      </div>
     </Fragment>
-    )
+  )
 }
 
 const ResponsiveDiv = styled.div`
@@ -94,7 +95,7 @@ const ResponsiveDiv = styled.div`
     display:none;
   }
 `
-const MobileDiv =styled.div`
+const MobileDiv = styled.div`
 @media only screen and (min-width: 1000px) {
     display:none;
   }
