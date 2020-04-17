0f218eb993b34e1b7328397cb1e584cd5a6f6b20
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-04-17 18:05:24 +0530

    Redesigned Events Page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,207 +6,226 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Slide from 'react-reveal/Slide';
+import { Container, CssBaseline } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import Events from '../assets/events.svg';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import Test from '../assets/undraw_scrum_board_cesn.svg'
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    width: '700px'
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    textAlign: 'center',
-    fontWeight: 'bold'
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  heading: {
-    color:'grey',
-    fontWeight: 'bold',
-  },
-  description: {
-      textAlign: 'center'
-  },
-  topics: {
-      fontWeight: 'bold',
 
-  },
-  list: {
-      color: 'grey',
-      fontSize: '1rem'
-  }
-});
 
-const Events = () =>  {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
 
-  return (
-      <div>
-      <div style={{paddingTop:'30px',
-      paddingLeft:'330px'}}><Typography variant='h4' className={classes.heading}>We have a history of conducting successful and useful events!</Typography></div>
-      <div style={{paddingTop:'50px', paddingLeft:'600px'}}><Typography variant='h4' className={classes.heading}>Upcoming Events!</Typography></div>
-      <br/>
-      <br/>
-      <div className='row' style={{paddingLeft:'50px'}}>
-      <div className='col s6'>
-      <Slide bottom>
-      <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} variant='h4' style={{paddingTop:'10px'}}>
-        Full Stack Bootcamp
-        </Typography>
-       <Typography className={classes.description} variant='h6' style={{paddingTop:'10px'}}>
-       We will have discussions and a problem solving oriented approach, we will have live youtube sessions, and a display of awesome projects at the end of the Bootcamp, something worth for the resume/CV. Additionally, for those who successfully submit their projects will get Google Developers Certified certificates and many more swags (pens, stickers, T-shirts, and whatnot). So what are you waiting for? Fill in the form and register yourself! See the attached flyer for the complete timeline!
-        </Typography>
-        <Typography className={classes.topics} variant='h6' style={{paddingTop:'10px'}}>
-        Topics Covered:
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Web Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Blockchain
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Software Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Machine Learning
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Data Structures and Algorithms
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-       IOT
-        </Typography>
-      </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button size="large" color='primary' variant="outlined">Register</Button>
+    button: {
+         backgroundColor: '#746B6B',
+         color:'white'
+    },
+    rootCard: {
+        maxWidth: 400,
+        height: 400
+      },
+      media: {
+        height: 230,
+      },
+      grid: {
+          height: 550
+      }
+  }));
+
+const EventsAlt = () => {
+    const classes = useStyles();
+    return (
+        <Fragment>
+         <CssBaseline />
+         <Container fixed>
+         <Grid container spacing={2} className={classes.grid}>
+         <Grid item xs={12} sm={6} md={3} lg={6} xl={6} style={{paddingTop:'100px',}}>
+             <Typography variant='h3' style={{fontWeight:'bold'}}>Events</Typography>
+             <Typography variant='h6' style={{color:'grey', paddingTop:'20px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+         </Grid>
+         <Grid item xs={12} sm={6} md={3} lg={6} xl={6} style={{paddingTop:'100px',}}>
+        <img src={Events} alt='event_img' height='50%' width='120%' />
+         </Grid>
+         </Grid>
+         <Grid container spacing={2}>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
       </CardActions>
     </Card>
     </Slide>
-    </div>
-    <div className='col s6'>
-    <Slide bottom>
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} variant='h4' style={{paddingTop:'10px'}}>
-        Full Stack Bootcamp
-        </Typography>
-       <Typography className={classes.description} variant='h6' style={{paddingTop:'10px'}}>
-       We will have discussions and a problem solving oriented approach, we will have live youtube sessions, and a display of awesome projects at the end of the Bootcamp, something worth for the resume/CV. Additionally, for those who successfully submit their projects will get Google Developers Certified certificates and many more swags (pens, stickers, T-shirts, and whatnot). So what are you waiting for? Fill in the form and register yourself! See the attached flyer for the complete timeline!
-        </Typography>
-        <Typography className={classes.topics} variant='h6' style={{paddingTop:'10px'}}>
-        Topics Covered:
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Web Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Blockchain
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Software Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Machine Learning
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Data Structures and Algorithms
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-       IOT
-        </Typography>
-      </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button size="large" color='primary' variant="outlined">Register</Button>
+         </Grid>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
       </CardActions>
     </Card>
     </Slide>
-    </div>
-    <div className='col s6' style={{paddingTop:'40px'}}>
-      <Slide bottom>
-      <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} variant='h4' style={{paddingTop:'10px'}}>
-        Full Stack Bootcamp
-        </Typography>
-       <Typography className={classes.description} variant='h6' style={{paddingTop:'10px'}}>
-       We will have discussions and a problem solving oriented approach, we will have live youtube sessions, and a display of awesome projects at the end of the Bootcamp, something worth for the resume/CV. Additionally, for those who successfully submit their projects will get Google Developers Certified certificates and many more swags (pens, stickers, T-shirts, and whatnot). So what are you waiting for? Fill in the form and register yourself! See the attached flyer for the complete timeline!
-        </Typography>
-        <Typography className={classes.topics} variant='h6' style={{paddingTop:'10px'}}>
-        Topics Covered:
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Web Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Blockchain
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Software Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Machine Learning
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Data Structures and Algorithms
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-       IOT
-        </Typography>
-      </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button size="large" color='primary' variant="outlined">Register</Button>
+         </Grid>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
       </CardActions>
     </Card>
     </Slide>
-    </div>
-    <div className='col s6' style={{paddingTop:'40px'}}>
-      <Slide bottom>
-      <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} variant='h4' style={{paddingTop:'10px'}}>
-        Full Stack Bootcamp
-        </Typography>
-       <Typography className={classes.description} variant='h6' style={{paddingTop:'10px'}}>
-       We will have discussions and a problem solving oriented approach, we will have live youtube sessions, and a display of awesome projects at the end of the Bootcamp, something worth for the resume/CV. Additionally, for those who successfully submit their projects will get Google Developers Certified certificates and many more swags (pens, stickers, T-shirts, and whatnot). So what are you waiting for? Fill in the form and register yourself! See the attached flyer for the complete timeline!
-        </Typography>
-        <Typography className={classes.topics} variant='h6' style={{paddingTop:'10px'}}>
-        Topics Covered:
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Web Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Blockchain
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Software Development
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Machine Learning
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-        Data Structures and Algorithms
-        </Typography>
-        <Typography className={classes.list} variant='h6' style={{paddingTop:'10px'}}>
-       IOT
-        </Typography>
-      </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button size="large" color='primary' variant="outlined">Register</Button>
+         </Grid>
+         </Grid>
+         <Grid container spacing={2}>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
       </CardActions>
     </Card>
     </Slide>
-    </div>
-    </div>
-    </div>
-  );
+         </Grid>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    </Slide>
+         </Grid>
+         <Grid item xs={12} sm={6} md={3} lg={4} xl={4} style={{}}>
+         <Slide bottom>
+         <Card className={classes.rootCard}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={Test}
+          title="Event"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    </Slide>
+         </Grid>
+         </Grid>
+         </Container>
+            
+        </Fragment>
+    )
 }
-export default Events;
+
+export default EventsAlt
