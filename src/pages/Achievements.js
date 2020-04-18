5a67547fa0a13b654bba8b701f5842735bbff30c
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-04-18 18:19:21 +0530

    Added modals in achievements page

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -11,7 +11,10 @@ import Grid from '@material-ui/core/Grid';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import Winner from '../assets/undraw_winners_ao2o.svg';
-import Test from '../assets/undraw_scrum_board_cesn.svg'
+import Test from '../assets/undraw_scrum_board_cesn.svg';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 
 
@@ -39,11 +42,32 @@ const useStyles = makeStyles((theme) => ({
       },
       grid: {
           height: 550
-      }
+      },
+      modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 400,
+        margin:'auto',
+      },
+      paperModal: {
+        backgroundColor: theme.palette.background.paper,
+        // border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
   }));
 
 const Achievements = () => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
         <Fragment>
          <CssBaseline />
@@ -78,7 +102,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -105,7 +129,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -132,7 +156,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -161,7 +185,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -188,7 +212,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -215,7 +239,7 @@ const Achievements = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -223,6 +247,26 @@ const Achievements = () => {
     </Slide>
          </Grid>
          </Grid>
+         <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+        
+      >
+        <Fade in={open}>
+          <div className={classes.paperModal}>
+            <h2 id="transition-modal-title">Lizard</h2>
+            <p id="transition-modal-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </Fade>
+      </Modal>
          </Container>
             
         </Fragment>
