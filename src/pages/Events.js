ee952074bb79b6eb37076fb6b098d08439c35709
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-04-18 18:10:11 +0530

    Added modal in events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -11,7 +11,10 @@ import Grid from '@material-ui/core/Grid';
 import Events from '../assets/events.svg';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
-import Test from '../assets/undraw_scrum_board_cesn.svg'
+import Test from '../assets/undraw_scrum_board_cesn.svg';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 
 
@@ -38,11 +41,32 @@ const useStyles = makeStyles((theme) => ({
       },
       grid: {
           height: 550
-      }
+      },
+      modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 400,
+        margin:'auto',
+      },
+      paperModal: {
+        backgroundColor: theme.palette.background.paper,
+        // border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
   }));
 
 const EventsAlt = () => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
         <Fragment>
          <CssBaseline />
@@ -77,7 +101,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -104,7 +128,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -131,7 +155,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -160,7 +184,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -187,7 +211,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -214,7 +238,7 @@ const EventsAlt = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleOpen}>
           Learn More
         </Button>
       </CardActions>
@@ -222,6 +246,26 @@ const EventsAlt = () => {
     </Slide>
          </Grid>
          </Grid>
+         <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+        
+      >
+        <Fade in={open}>
+          <div className={classes.paperModal}>
+            <h2 id="transition-modal-title">Lizard</h2>
+            <p id="transition-modal-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </Fade>
+      </Modal>
          </Container>
             
         </Fragment>
